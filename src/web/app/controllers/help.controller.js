bf6c17542300b5cbb0a1e3a390434489fca4301a
juanvidev <juanvidev1@gmail.com>
2025-11-09 13:32:02 -0500

    Add OPEN_ROUTER_TOKEN command to help commands list

--- src/web/app/controllers/help.controller.js
+++ src/web/app/controllers/help.controller.js
@@ -14,6 +14,11 @@ class HelpController {
         command: '/consultar_gastos',
         description: 'Consultar gastos registrados',
       },
+      {
+        command: 'OPEN_ROUTER_TOKEN',
+        description:
+          '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',
+      },
     ];
     res.json(commands);
   };
